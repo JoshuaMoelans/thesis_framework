3670954b8e058e3bcdcf7d31929ce48d65e1dbee
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2024-04-29 13:06:09 +0200

    First MLSL run - results seem good!

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     game_location = r"./game/game.exe"
     logs_location = r"C:\Users\Joshua\AppData\Roaming\Godot\app_userdata\Godot Exploration - 2d\logs"
     game = GameOptimizer(opt_algo=GN_MLSL, opt_algo_2=LN_COBYLA, game_location=game_location, logs_location=logs_location, ingame_instance_count=16, timeout=35)
-    game = GameOptimizer(opt_algo=LN_COBYLA, game_location=game_location, logs_location=logs_location, ingame_instance_count=16, timeout=35)
+    # game = GameOptimizer(opt_algo=LN_COBYLA, game_location=game_location, logs_location=logs_location, ingame_instance_count=16, timeout=35)
     # test_score_reading(game) # TODO restructure this test
     
     game.optimize(xtol_rel=0.1, ftol_rel=0.1, maxeval=30)
